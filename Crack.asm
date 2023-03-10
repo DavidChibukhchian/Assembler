1fecac0da9b78433798f8e0ed2251c00ea2421a8
DavidChibukhchian <111795181+DavidChibukhchian@users.noreply.github.com>
2023-03-10 06:09:19 +0300

    Add files via upload

---
+++ Crack.asm
@@ -0,0 +1,177 @@
+.model tiny
+.code
+org 100h
+locals @@
+
+Start:              mov bx, 0B800h
+                    mov es, bx
+
+                    call Display_Menu
+                    call Enter_Password
+                    call Verify_Password
+
+                    mov ax, 4C00h
+                    int 21h
+
+
+
+;-----------------------------------------------------------
+; Displays the start menu of this HackMe program
+;-----------------------------------------------------------
+; Entry:            None
+; Expects:          ES -> video segment
+; Destroys:         AX, BX, CX, DX, SI
+; Exit:             None
+;-----------------------------------------------------------
+Display_Menu        proc
+
+                    mov bx, 160d + 2d
+                    mov ch, 5Ch                                 ; color of a frame symbol
+                    mov cl, 5Ch                                 ; color of a space symbol
+                    mov dh, 13d								    ; height of the frame
+					mov dl, 38d								    ; width  of the frame
+                    call DrawFrame
+
+                    mov ah, 3Ch                                 ; color of a symbol for the picture
+                    mov al, ' '                                 ; symbol for the picture
+                    call Print_HACK_ME
+
+                    mov bx, 160*9d + 2*10d
+                    mov si, offset Enter_The_Password
+                    mov ch, 4Ah                                 ; color for "ENTER THE PASSWORD"
+                    call PrintString
+                
+                    mov field_coordinates, 160*11d + 2*14d      ; saves password field coordinates
+                    mov bx, field_coordinates
+
+                    mov si, offset Password_Field
+                    mov ch, 3Ch                                 ; color for a field to entering the password
+                    call PrintString
+
+                    ret
+                    endp
+;-----------------------------------------------------------
+
+
+
+;-----------------------------------------------------------
+; Records entered password to a password buffer
+; Shows a blinking underscore and stars when entering
+;-----------------------------------------------------------
+; Entry:            None
+; Expects:          ES -> video segment
+; Destroys:         AX, BX, CX, DX, SI
+; Exit:             None
+;-----------------------------------------------------------
+max_len_of_password EQU 12
+
+Enter_Password      proc
+
+                    mov bx, field_coordinates
+                    mov ch, 0BAh                                ; prints the first blinking underscore
+                    mov cl, '_'
+                    mov es:[bx], cx
+                    
+                    mov si, offset Entered_Password
+                    xor dx, dx
+
+@@Next_Symbol:      mov ah, 08h                                 ; reads a symbol without echo
+                    int 21h
+                    inc dx
+
+                    cmp al, 0Dh                                 ; compares with enter
+                    je @@Enter_Exit
+
+                    mov [si], al                                ; puts entered char to a password buffer
+                    inc si
+
+                    mov ch, 3Ah
+                    mov cl, '*'
+                    mov es:[bx], cx                             ; prints a star
+                    add bx, 2
+                    mov ch, 0BAh
+
+                    cmp dx, max_len_of_password
+                    je @@Max_Len_Exit
+
+                    mov cl, '_'                                 ; prints a blinking underscore
+                    mov es:[bx], cx
+
+                    jmp @@Next_Symbol
+
+@@Max_Len_Exit:     mov ah, 08h                         
+                    int 21h
+                    mov [si], al
+
+                    cmp al, 0Dh                                 ; continues only by enter
+                    jne @@Max_Len_Exit
+                    jmp @@Exit
+
+@@Enter_Exit:       mov cl, ' '                                 ; does not print a blinking star 
+                    mov es:[bx], cx
+
+@@Exit:             ret
+                    endp
+;-----------------------------------------------------------
+
+
+
+;-----------------------------------------------------------
+; Verifies entered password and displays
+; an appropriate picture
+;-----------------------------------------------------------
+; Entry:            None
+; Expects:          ES -> video segment
+; Destroys:         AX, BX, CX, DX, SI, DI
+; Exit:             None
+;-----------------------------------------------------------
+Verify_Password     proc
+
+                    mov si, offset Entered_Password
+                    mov di, offset Right_Password
+
+                    mov cx, max_len_of_password
+                    mov dh, [si]                                ; entered symbols will be in DH
+
+@@Next:             cmp dh, [di]                                ; comparison
+                    jne @@Change_Flag
+
+                    inc si
+                    mov dh, [si]
+                    inc di
+                    loop @@Next
+                    
+                    jmp @@Result
+
+@@Change_Flag:      add flag, 1
+                    
+@@Result:           mov ah, 93h                                 ; color 
+                    mov al, 0B1h                                ; a symbol for the picture
+
+                    cmp flag, 1                                 ; flag = 0 - right password
+                    je @@AccessDenied                           ; flag = 1 - wrong password
+
+@@AccessGranted:    call Print_ACCESS_GRANTED
+                    jmp @@Exit
+
+@@AccessDenied:     call Print_ACCESS_DENIED
+
+@@Exit:             ret
+                    endp
+;-----------------------------------------------------------
+
+include CrackLib.asm
+
+;-----------------------------------------------------------
+
+Enter_The_Password  db ' ENTER THE PASSWORD ', 0
+Password_Field      db max_len_of_password DUP(' '), 0
+field_coordinates   dw 0
+
+Right_Password      db 'hola', 8 DUP(0)
+Entered_Password    db max_len_of_password DUP(0)
+flag                db 0
+
+;-----------------------------------------------------------
+
+end                 Start
---
+++ CrackLib.asm
@@ -0,0 +1,459 @@
+;-----------------------------------------------------------
+; Draws a frame on the screen
+;-----------------------------------------------------------
+; Entry:			BX = video segment coordinates
+;					CH = color attributes of frame symbol
+;					CL = color attributes of space symbol		
+;					DH = height
+;					DL = width
+; Expects:			ES -> video segment
+; Destroys:			AX
+; Exit:				None
+;-----------------------------------------------------------
+new_line 			EQU 160d
+
+DrawFrame			proc
+					push dx bx cx
+
+					mov ah, 0C9h
+					mov al, 0CDh
+					mov cl, ch
+					call DrawLine
+					pop cx
+
+					push bx dx
+					xor dh, dh
+					add bx, dx
+					add bx, dx
+					sub bx, 2
+					mov byte ptr es:[bx], 0BBh
+					pop dx bx
+
+					add bx, new_line
+					dec dh 		
+
+					mov ah, 0BAh								;
+					mov al, ' '
+		
+@@Next_Line:		cmp dh, 1
+					je @@Exit
+
+					call DrawLine
+					add bx, new_line
+					dec dh
+					jmp @@Next_Line
+
+@@Exit:				push cx
+					mov cl, ch
+					mov ah, 0C8h
+					mov al, 0CDh
+					call DrawLine
+					pop cx
+					
+					xor dh, dh
+					add bx, dx
+					add bx, dx
+					sub bx, 2
+					mov byte ptr es:[bx], 0BCh
+
+					pop bx dx
+					ret
+					endp
+;-----------------------------------------------------------
+
+
+
+;-----------------------------------------------------------
+; Draws a line on the screen
+;-----------------------------------------------------------
+; Entry:			AH = frame symbol
+;					AL = space symbol
+;					BX = video segment coordinates
+;					CH = color of the frame symbol
+;					CL = color of the space symbol
+;					DL = width
+; Expects:			ES -> video segment
+; Destroys:			None
+; Exit:				None	
+;-----------------------------------------------------------
+size_of_pixel 		EQU 2
+
+DrawLine			proc
+					push bx dx
+
+					sub dl, size_of_pixel
+
+					mov byte ptr es:[bx],   ah
+					mov byte ptr es:[bx+1], ch
+
+@@Next_Symbol:		cmp dl, 0
+					je @@Exit
+
+					add bx, size_of_pixel
+					mov byte ptr es:[bx],   al
+					mov byte ptr es:[bx+1], cl
+					dec dl
+					jmp @@Next_Symbol
+		
+@@Exit:				add bx, size_of_pixel
+					mov byte ptr es:[bx],   ah
+					mov byte ptr es:[bx+1], ch
+
+					pop dx bx
+					ret
+					endp
+;-----------------------------------------------------------
+
+
+
+;-----------------------------------------------------------
+; Prints a string on the screen
+;-----------------------------------------------------------
+; Entry: 			BX = video segment coordinates
+;					CH = color
+;					SI = address of string
+; Expects:			ES -> video segment
+; Destroys:			CL, SI
+; Exit:				None
+;-----------------------------------------------------------
+PrintString			proc
+
+					push bx
+
+@@Next:				mov cl, [si]
+					cmp cl, 0
+					je @@Exit
+
+					mov byte ptr es:[bx],   cl
+					mov byte ptr es:[bx+1], ch
+
+					add bx, 2
+					inc si
+					jmp @@Next
+
+@@Exit:	 			pop bx
+					ret
+					endp				
+;-----------------------------------------------------------
+
+
+
+;-----------------------------------------------------------
+; Draws "HACK ME" in the frame
+;-----------------------------------------------------------
+; Entry:			AH = color 
+;					AL = symbol
+; Expects:			ES -> video segment
+; Destroys:			BX
+; Exit:				None
+;-----------------------------------------------------------
+Print_HACK_ME		proc
+
+                    mov bx, 160* 3d + 2d
+                    mov es:[bx+2*3],  ax
+                    mov es:[bx+2*6],  ax
+                    mov es:[bx+2*9],  ax
+                    mov es:[bx+2*10], ax
+                    mov es:[bx+2*13], ax
+                    mov es:[bx+2*14], ax
+                    mov es:[bx+2*15], ax
+                    mov es:[bx+2*16], ax
+                    mov es:[bx+2*18], ax
+                    mov es:[bx+2*21], ax
+                    mov es:[bx+2*25], ax
+                    mov es:[bx+2*29], ax
+                    mov es:[bx+2*31], ax
+                    mov es:[bx+2*32], ax
+                    mov es:[bx+2*33], ax
+                    mov es:[bx+2*34], ax
+
+                    add bx, 160d
+                    mov es:[bx+2*3],  ax
+                    mov es:[bx+2*6],  ax
+                    mov es:[bx+2*8],  ax
+                    mov es:[bx+2*11], ax
+                    mov es:[bx+2*13], ax
+                    mov es:[bx+2*18], ax
+                    mov es:[bx+2*20], ax
+                    mov es:[bx+2*25], ax
+                    mov es:[bx+2*26], ax
+                    mov es:[bx+2*28], ax
+                    mov es:[bx+2*29], ax
+                    mov es:[bx+2*31], ax
+
+                    add bx, 160d
+                    mov es:[bx+2*3],  ax
+                    mov es:[bx+2*4],  ax
+                    mov es:[bx+2*5],  ax
+                    mov es:[bx+2*6],  ax
+                    mov es:[bx+2*8],  ax
+                    mov es:[bx+2*9],  ax
+                    mov es:[bx+2*10], ax
+                    mov es:[bx+2*11], ax
+                    mov es:[bx+2*13], ax
+                    mov es:[bx+2*18], ax
+                    mov es:[bx+2*19], ax
+                    mov es:[bx+2*25], ax
+                    mov es:[bx+2*27], ax
+                    mov es:[bx+2*29], ax
+                    mov es:[bx+2*31], ax
+                    mov es:[bx+2*32], ax
+                    mov es:[bx+2*33], ax
+                    mov es:[bx+2*34], ax
+
+                    add bx, 160d
+                    mov es:[bx+2*3],  ax
+                    mov es:[bx+2*6],  ax
+                    mov es:[bx+2*8],  ax
+                    mov es:[bx+2*11], ax
+                    mov es:[bx+2*13], ax
+                    mov es:[bx+2*18], ax
+                    mov es:[bx+2*20], ax
+                    mov es:[bx+2*25], ax
+                    mov es:[bx+2*29], ax
+                    mov es:[bx+2*31], ax
+
+                    add bx, 160d
+                    mov es:[bx+2*3],  ax
+                    mov es:[bx+2*6],  ax
+                    mov es:[bx+2*8],  ax
+                    mov es:[bx+2*11], ax
+                    mov es:[bx+2*13], ax
+                    mov es:[bx+2*14], ax
+                    mov es:[bx+2*15], ax
+                    mov es:[bx+2*16], ax
+                    mov es:[bx+2*18], ax
+                    mov es:[bx+2*21], ax
+                    mov es:[bx+2*25], ax
+                    mov es:[bx+2*29], ax
+                    mov es:[bx+2*31], ax
+                    mov es:[bx+2*32], ax
+                    mov es:[bx+2*33], ax
+                    mov es:[bx+2*34], ax
+
+					ret
+					endp
+;-----------------------------------------------------------
+
+
+
+;-----------------------------------------------------------
+; Draws a picture in case of right password
+;-----------------------------------------------------------
+; Entry:			AH = color 
+;					AL = symbol
+; Expects:			ES -> video segment
+; Destroys:			BX, CX, SI
+; Exit:				None
+;-----------------------------------------------------------
+Print_ACCESS_GRANTED proc
+
+					push ax
+
+					mov bx, 160d + 2d
+                    mov ch, 5Ch
+                    mov cl, 27h
+                    mov dh, 13
+                    mov dl, 38
+                    call DrawFrame
+
+                    mov si, offset AccessGranted
+                    mov bx, 160*4d + 2*13d
+                    mov ch, 3Ah
+                    call PrintString
+
+					pop ax
+                    mov bx, 160*6d
+
+                    mov es:[bx+2*7],  ax
+                    mov es:[bx+2*11], ax
+                    mov es:[bx+2*13], ax
+                    mov es:[bx+2*14], ax
+                    mov es:[bx+2*15], ax
+                    mov es:[bx+2*16], ax
+                    mov es:[bx+2*18], ax
+                    mov es:[bx+2*19], ax
+                    mov es:[bx+2*20], ax
+                    mov es:[bx+2*21], ax
+                    mov es:[bx+2*23], ax
+                    mov es:[bx+2*24], ax
+                    mov es:[bx+2*25], ax
+                    mov es:[bx+2*26], ax
+                    mov es:[bx+2*28], ax
+                    mov es:[bx+2*30], ax
+                    mov es:[bx+2*32], ax
+
+                    add bx, 160d
+                    mov es:[bx+2*8],  ax
+                    mov es:[bx+2*9],  ax
+                    mov es:[bx+2*10], ax
+                    mov es:[bx+2*13], ax
+                    mov es:[bx+2*16], ax
+                    mov es:[bx+2*18], ax
+                    mov es:[bx+2*21], ax
+                    mov es:[bx+2*23], ax
+                    mov es:[bx+2*26], ax
+                    mov es:[bx+2*28], ax
+                    mov es:[bx+2*30], ax
+                    mov es:[bx+2*32], ax
+
+                    add bx, 160d
+                    mov es:[bx+2*9],  ax
+                    mov es:[bx+2*13], ax
+                    mov es:[bx+2*16], ax
+                    mov es:[bx+2*18], ax
+                    mov es:[bx+2*19], ax
+                    mov es:[bx+2*20], ax
+                    mov es:[bx+2*21], ax
+                    mov es:[bx+2*23], ax
+                    mov es:[bx+2*26], ax
+                    mov es:[bx+2*28], ax
+                    mov es:[bx+2*30], ax
+                    mov es:[bx+2*32], ax
+
+                    add bx, 160d
+                    mov es:[bx+2*8],  ax
+                    mov es:[bx+2*9],  ax
+                    mov es:[bx+2*10], ax
+                    mov es:[bx+2*13], ax
+                    mov es:[bx+2*16], ax
+                    mov es:[bx+2*18], ax
+                    mov es:[bx+2*23], ax
+                    mov es:[bx+2*26], ax
+                    mov es:[bx+2*28], ax
+                    mov es:[bx+2*30], ax
+                    mov es:[bx+2*32], ax
+
+                    add bx, 160d
+                    mov es:[bx+2*7],  ax
+                    mov es:[bx+2*11], ax
+                    mov es:[bx+2*13], ax
+                    mov es:[bx+2*14], ax
+                    mov es:[bx+2*15], ax
+                    mov es:[bx+2*16], ax
+                    mov es:[bx+2*18], ax
+                    mov es:[bx+2*23], ax
+                    mov es:[bx+2*24], ax
+                    mov es:[bx+2*25], ax
+                    mov es:[bx+2*26], ax
+                    mov es:[bx+2*28], ax
+                    mov es:[bx+2*29], ax
+                    mov es:[bx+2*30], ax
+                    mov es:[bx+2*31], ax
+                    mov es:[bx+2*32], ax
+
+					ret
+					endp
+;-----------------------------------------------------------
+
+
+
+;-----------------------------------------------------------
+; Draws a picture in case of wrong password
+;-----------------------------------------------------------
+; Entry:			AH = color
+;					AL = symbol
+; Expects:			ES -> video segment
+; Destroys:			AX, BX, CX, DX, ES, SI, DI
+; Exit:				None
+;-----------------------------------------------------------
+Print_ACCESS_DENIED	proc
+
+					push ax
+
+					mov bx, 160d + 2d
+                    mov ch, 5Ch
+                    mov cl, 47h
+                    mov dh, 13
+                    mov dl, 38
+                    call DrawFrame
+
+                    mov si, offset AccessDenied
+                    mov bx, 160*4d + 2*13d
+                    mov ch, 3Ch
+                    call PrintString
+
+                    pop ax
+					mov bx, 160*6d
+
+                    mov es:[bx+2*8],  ax
+                    mov es:[bx+2*13], ax
+                    mov es:[bx+2*14], ax
+                    mov es:[bx+2*15], ax
+                    mov es:[bx+2*16], ax
+                    mov es:[bx+2*18], ax
+                    mov es:[bx+2*19], ax
+                    mov es:[bx+2*20], ax
+                    mov es:[bx+2*21], ax
+                    mov es:[bx+2*23], ax
+                    mov es:[bx+2*24], ax
+                    mov es:[bx+2*25], ax
+                    mov es:[bx+2*26], ax
+                    mov es:[bx+2*28], ax
+                    mov es:[bx+2*29], ax
+                    mov es:[bx+2*30], ax
+                    mov es:[bx+2*31], ax
+
+                    add bx, 160d
+                    mov es:[bx+2*8],  ax
+                    mov es:[bx+2*13], ax
+                    mov es:[bx+2*16], ax
+                    mov es:[bx+2*18], ax
+                    mov es:[bx+2*23], ax
+                    mov es:[bx+2*28], ax
+                    mov es:[bx+2*31], ax
+
+                    add bx, 160d
+                    mov es:[bx+2*8],  ax
+                    mov es:[bx+2*13], ax
+                    mov es:[bx+2*16], ax
+                    mov es:[bx+2*18], ax
+                    mov es:[bx+2*19], ax
+                    mov es:[bx+2*20], ax
+                    mov es:[bx+2*21], ax
+                    mov es:[bx+2*23], ax
+                    mov es:[bx+2*24], ax
+                    mov es:[bx+2*25], ax
+                    mov es:[bx+2*26], ax
+                    mov es:[bx+2*28], ax
+                    mov es:[bx+2*29], ax
+                    mov es:[bx+2*30], ax
+                    mov es:[bx+2*31], ax
+
+                    add bx, 160d
+                    mov es:[bx+2*8],  ax
+                    mov es:[bx+2*13], ax
+                    mov es:[bx+2*16], ax
+                    mov es:[bx+2*21], ax
+                    mov es:[bx+2*23], ax
+                    mov es:[bx+2*28], ax
+                    mov es:[bx+2*30], ax
+
+                    add bx, 160d
+                    mov es:[bx+2*8],  ax
+                    mov es:[bx+2*9],  ax
+                    mov es:[bx+2*10], ax
+                    mov es:[bx+2*11], ax
+                    mov es:[bx+2*13], ax
+                    mov es:[bx+2*14], ax
+                    mov es:[bx+2*15], ax
+                    mov es:[bx+2*16], ax
+                    mov es:[bx+2*18], ax
+                    mov es:[bx+2*19], ax
+                    mov es:[bx+2*20], ax
+                    mov es:[bx+2*21], ax
+                    mov es:[bx+2*23], ax
+                    mov es:[bx+2*24], ax
+                    mov es:[bx+2*25], ax
+                    mov es:[bx+2*26], ax
+                    mov es:[bx+2*28], ax
+                    mov es:[bx+2*31], ax
+
+					ret
+					endp
+;-----------------------------------------------------------
+
+AccessGranted       db 'ACCESS GRANTED', 0
+AccessDenied        db 'ACCESS  DENIED', 0
+
+;-----------------------------------------------------------
