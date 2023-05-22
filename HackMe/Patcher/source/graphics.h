54bf2de75447058b23b7b06e28ac5bc6263820d8
David_Chibukhchian <david.chibuhchyan1@gmail.com>
2023-05-22 11:21:55 +0300

    Add patcher source files and SFML library

---
+++ HackMe/Patcher/source/graphics.h
@@ -0,0 +1,58 @@
+#ifndef _GRAPHICS_H
+#define _GRAPHICS_H
+
+
+//-----------------------------------------------------------------------------------------------------------
+
+
+#define HEIGHT_OF_WINDOW        400
+#define WIDTH_OF_WINDOW         600
+#define NUMBER_OF_MENU_BUTTONS  3
+
+
+//-----------------------------------------------------------------------------------------------------------
+
+
+#include <SFML/Graphics.hpp>
+#include <stdio.h>
+#include <time.h>
+#include "patcher.h"
+
+
+//-----------------------------------------------------------------------------------------------------------
+
+
+enum graphics_Errors
+{
+    Failed_To_Load_Music            = 1,
+    Failed_To_Load_Font             = 2,
+    Failed_To_Load_Main_Texture     = 3,
+    Failed_To_Load_Patching_Texture = 4,
+    Failed_To_Load_About_Texture    = 5
+};
+
+
+enum Menu_Buttons
+{
+    START = 0,
+    ABOUT = 1,
+    EXIT  = 2
+};
+
+
+//-----------------------------------------------------------------------------------------------------------
+
+
+void init_text(sf::Text* text, sf::Font* font, const char* str, size_t size, sf::Color color);
+
+void init_menu_buttons(sf::Text* menu_button, sf::Font* font);
+
+void change_color_of_menu_buttons(sf::Text* menu_button, size_t selected_button);
+
+void patch_program(sf::RenderWindow* window, sf::Texture* texture, sf::Sprite* sprite, sf::Text* title, sf::Font* font);
+
+
+//-----------------------------------------------------------------------------------------------------------
+
+
+#endif // _GRAPHICS_H
---
+++ HackMe/Patcher/source/patcher.cpp
@@ -0,0 +1,56 @@
+#include "patcher.h"
+
+
+//-----------------------------------------------------------------------------------------------------------
+
+
+static size_t jump_address_1 = 63;
+static size_t new_byte_1     = 41;
+
+static size_t jump_address_2 = 93;
+static size_t new_byte_2     = 11;
+
+
+//-----------------------------------------------------------------------------------------------------------
+
+
+static size_t get_file_size(FILE* filename)
+{
+    struct stat file_data = {};
+    fstat(fileno(filename), &file_data);
+
+    return file_data.st_size;
+}
+
+
+//-----------------------------------------------------------------------------------------------------------
+
+
+int patch_hackme()
+{
+    FILE* target = fopen("HACKME.com", "r+b");
+    if (target == nullptr)
+    {
+        return Failed_To_Open_Target_File;
+    }
+
+    size_t file_size = get_file_size(target);
+
+    if (file_size == 0)
+    {
+        return Target_File_Is_Empty;
+    }
+
+    fseek(target, jump_address_1, SEEK_SET);
+    fputc(new_byte_1, target);
+
+    fseek(target, jump_address_2, SEEK_SET);
+    fputc(new_byte_2, target);
+
+    fclose(target);
+
+    return 0;
+}
+
+
+//-----------------------------------------------------------------------------------------------------------
